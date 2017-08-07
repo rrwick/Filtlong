c54e2ce9eadd179090e2a28b744f8fa4606a3f14
Ryan Wick <rrwick@gmail.com>
2017-08-07 11:27:41 +1000

    Reformat license

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,19 +1,17 @@
-//Copyright 2017 Ryan Wick
+// Copyright 2017 Ryan Wick
 
-//This file is part of LongQC
+// This file is part of LongQC
 
-//LongQC is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+// LongQC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
 
-//LongQC is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// LongQC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
 
-//You should have received a copy of the GNU General Public License
-//along with LongQC.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with LongQC.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 
 #include "misc.h"
